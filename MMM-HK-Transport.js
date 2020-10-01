e9c97c516134fd6aae578b11357487cf7504c389
root <winstonma@ymail.com>
2020-10-01 10:16:33 +0000

    Remove the comma between english words

--- MMM-HK-Transport.js
+++ MMM-HK-Transport.js
@@ -95,9 +95,10 @@ Module.register("MMM-HK-Transport", {
         }
 
         const REGEX_CHINESE = /[\u4e00-\u9fff]|[\u3400-\u4dbf]|[\u{20000}-\u{2a6df}]|[\u{2a700}-\u{2b73f}]|[\u{2b740}-\u{2b81f}]|[\u{2b820}-\u{2ceaf}]|[\uf900-\ufaff]|[\u3300-\u33ff]|[\ufe30-\ufe4f]|[\uf900-\ufaff]|[\u{2f800}-\u{2fa1f}]/u;
-        const splitStr = input.split(' ')
-        const chiWords = splitStr.filter((string) => REGEX_CHINESE.test(string))
-        const engWords = splitStr.filter((string) => !REGEX_CHINESE.test(string))
+        const splitStr = input.split(' ');
+        debugger
+        const chiWords = splitStr.filter((string) => REGEX_CHINESE.test(string));
+        const engWords = splitStr.filter((string) => !REGEX_CHINESE.test(string)).slice(1).join(' ');
 
         if (langTable[config.language])
             return chiWords;
