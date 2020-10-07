ac89d0d927eb31d7fd1e81f76bedfb23b108e22c
root <winstonma@ymail.com>
2020-10-07 09:29:43 +0000

    Update doc and use default logger

--- node_helper.js
+++ node_helper.js
@@ -2,92 +2,91 @@
  * Module: MMM-HK-Transport
  *
  * By Winston / https://github.com/winstonma
- * MIT Licensed.
+ * AGPL-3.0 Licensed.
  */
 
 const validUrl = require("valid-url");
 const ETAFetcher = require("./etafetcher.js");
 const NodeHelper = require("node_helper");
 const got = require('got');
+const Log = require("../../js/logger");
 
 module.exports = NodeHelper.create({
-
+    // Override start method.
     start: function () {
-        console.log("Starting node helper for: " + this.name);
+        Log.log("Starting node helper for: " + this.name);
         this.fetchers = [];
     },
 
+    // Override socketNotificationReceived received.
     socketNotificationReceived: function (notification, payload) {
-        // Request for information
         if (notification === 'ADD_STOP') {
             this.getStopInfo(payload.stop, payload.config);
             return;
         }
     },
 
-    /* getStopInfo(stopInfo, config)
+    /**
      * Obtain the stop id from the URL
      *
-     * attribute stopInfo object - A stopInfo object.
-     * attribute config object - A configuration object containing reload interval in milliseconds.
+     * @param {object} stopInfo The stopInfo.
+     * @param {object} config The configuration object.
      */
     getStopInfo: function (stopInfo, config) {
-        const self = this;
         const baseURL = "https://citymapper.com/api/3/stopinfo?region_id=hk-hongkong&ids=";
 
         const url = baseURL + stopInfo.stopID;
         (async () => {
             try {
-                const response = await got(url, {
+                const {body} = await got(url, {
                     responseType: 'json'
                 });
-                this.createFetcher(response.body.stops[0].id, config);
+                this.createFetcher(body.stops[0].id, config);
             } catch (error) {
-                console.log(error.response.body);
+                this.sendSocketNotification("FETCH_ERROR", {
+                    stopID: stopInfo.stopID,
+                    error: error
+                });
             }
         })();
     },
 
-    /* createFetcher(feed, config)
-     * Creates a fetcher for a new feed if it doesn't exist yet.
+    /**
+     * Creates a fetcher for a ETA feed if it doesn't exist yet.
      * Otherwise it reuses the existing one.
      *
-     * attribute feed object - A feed object.
-     * attribute config object - A configuration object containing reload interval in milliseconds.
+     * @param {object} stopID The stopID.
+     * @param {object} config The configuration object.
      */
     createFetcher: function (stopID, config) {
-        var self = this;
-
-        var url = config.cityMapperURL + stopID;
-        var url = url || "";
-        var reloadInterval = config.reloadInterval || 5 * 60 * 1000;
+        const url = config.cityMapperURL + stopID || "";
+        const reloadInterval = config.reloadInterval || 5 * 60 * 1000;
 
         if (!validUrl.isUri(url)) {
-            console.log("invalid url");
-            self.sendSocketNotification("INCORRECT_URL", url);
+            this.sendSocketNotification("INCORRECT_URL", url);
             return;
         }
 
-        var fetcher;
-        if (typeof self.fetchers[stopID] === "undefined") {
-            console.log("Create new CityMapper fetcher for url: " + url + " - Interval: " + reloadInterval);
+        let fetcher;
+        if (typeof this.fetchers[stopID] === "undefined") {
+            Log.log("Create new CityMapper fetcher for url: " + url + " - Interval: " + reloadInterval);
             fetcher = new ETAFetcher(url, reloadInterval);
 
-            fetcher.onReceive(function (fetcher) {
-                self.broadcastFeeds();
-            });
+            fetcher.onReceive(() => {
+				this.broadcastFeeds();
+			});
 
-            fetcher.onError(function (fetcher, error) {
-                self.sendSocketNotification("FETCH_ERROR", {
+            fetcher.onError((fetcher, error) => {
+                this.sendSocketNotification("FETCH_ERROR", {
                     url: fetcher.url(),
                     error: error
                 });
             });
 
-            self.fetchers[stopID] = fetcher;
+            this.fetchers[stopID] = fetcher;
         } else {
-            console.log("Use existing CityMapper fetcher for url: " + url);
-            fetcher = self.fetchers[stopID];
+            Log.log("Use existing CityMapper fetcher for url: " + url);
+            fetcher = this.fetchers[stopID];
             fetcher.setReloadInterval(reloadInterval);
             fetcher.broadcastItems();
         }
@@ -95,14 +94,12 @@ module.exports = NodeHelper.create({
         fetcher.startFetch();
     },
 
-    /* broadcastFeeds()
-     * Creates an object with all feed items of the different registered feeds,
-     * and broadcasts these using sendSocketNotification.
-     */
+	/**
+	 * Creates an object with all ETA items of the different registered stopID,
+	 * and broadcasts these using sendSocketNotification.
+	 */
     broadcastFeeds: function () {
-        const self = this;
-
-        const feeds = Object.entries(self.fetchers)
+        const feeds = Object.entries(this.fetchers)
             .map(([key, fetcher]) => [key, fetcher.items()])
             .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
 
