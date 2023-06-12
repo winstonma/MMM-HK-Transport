4880ae01285a4f26286e6508262091565f7065e0
Nisnis39 <63162269+Nisnis39@users.noreply.github.com>
2023-06-12 23:24:36 +0200

    Update MMM-HK-Transport.js

--- MMM-HK-Transport.js
+++ MMM-HK-Transport.js
@@ -10,7 +10,7 @@ Module.register("MMM-HK-Transport", {
     defaults: {
         stops: [
             {
-                stopID: 'HKStop_KowloonCentralPostOffice_N_1'
+                stopID: 'STIF:StopPoint:Q:473921:'
             }
         ],
         timeFormat: (config.timeFormat !== 24) ? "h:mm" : "HH:mm",
